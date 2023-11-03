434368eb16c759b7b0a40a830aae20d1e48f6808
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2023-11-03 21:51:05 +0530

    some changes

--- React Course/Learning 3/App.js
+++ React Course/Learning 3/App.js
@@ -1,18 +1,10 @@
 
-{/* <div id = "parent">
-        <div id = "child1">
-            <h1>I am h1 tag</h1> //sibling 1
-            <h2>I am h2 tag </h2> //sibling 2
-        </div>
-        <div id = "child2">
-            <h1>I am h1 tag</h1> //sibling 1
-            <h2>I am h2 tag </h2> //sibling 2
-        </div>
-    </div> */}
-                        //           tag    attribute    children div nesting h1 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child1" }, [   //use array for siblings
-        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "This is Binit🚀"),
         React.createElement("h2", {}, "I am h2 tag")
     ]),
     React.createElement("div", { id: "child2" }, [   //use array for siblings
@@ -21,37 +13,5 @@ const parent = React.createElement("div", { id: "parent" }, [
 ])
 ]);
 
-
-
-
-
-// code 2------------------->
-// {/* <div id = "parent">
-//         <div id = "child">
-//             <h1>I am h1 tag</h1> //sibling 1
-//             <h2>I am h2 tag </h2> //sibling 2
-//     </div>
-// </div> */}
-//                         //           tag    attribute    children div nesting h1 
-// const parent = React.createElement("div", { id: "parent" }, 
-//     React.createElement("div", { id: "child" }, [   //use array for siblings
-//         React.createElement("h1", {}, "I am h1 tag"),
-//         React.createElement("h2", {}, "I am h2 tag")]
-//     )
-// );
-
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-
-
-
-// Code 1 ----------------->
-              // React.createElement(tags,attribute, children)
-// let heading = React.createElement("h1", {id : "heading" }, "Hello World from React!");
-
-// let root = ReactDOM.createRoot(document.getElementById("root"));
-
-// console.log(heading); //returns object
-
-// root.render(heading);
